2eeb2089f41b32707f1886dabab1acffad70eef8
Justin J Russell <justinjrussell001@gmail.com>
2018-01-12 10:12:19 -0800

    Removed take(2)

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -19,7 +19,7 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming().take(2) {
+    for stream in listener.incoming() {
         let stream = stream.unwrap();
         println!("Connection eastablished!");
                 
